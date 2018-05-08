088bd3a2e80c310890b472d9cd38d43f51ecf2bf
Gustavo Primolan <gustavo.primolan@hotmail.com>
2018-05-08 00:08:19 -0300

    Banco de dados H2 e a criacao automatica da base de dados

--- projetos/cursomc/src/main/java/br/com/cursomc/resources/CategoriaResource.java
+++ projetos/cursomc/src/main/java/br/com/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package br.com.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cursomc.domain.Categoria;
 
 //CONTROLADOR REST E RESPONDERÁ PELO END POINT /CATEGORIAS
 @RestController
@@ -11,8 +16,16 @@ public class CategoriaResource {
 	
 	//CAIRÁ AQUI QUANDO UTILIZAR O /categorias NO GET
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
