64b138f5428730960a26c98b7c490db5a4b20a59
Gustavo Primolan <gustavo.primolan@hotmail.com>
2018-05-16 20:07:18 -0300

    Criando repository e service para categoria

--- projetos/cursomc/src/main/java/br/com/cursomc/resources/CategoriaResource.java
+++ projetos/cursomc/src/main/java/br/com/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,33 @@ package br.com.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.services.CategoriaService;
 
 //CONTROLADOR REST E RESPONDERÁ PELO END POINT /CATEGORIAS
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	//CAIRÁ AQUI QUANDO UTILIZAR O /categorias NO GET
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	//O ResponseEntity ENCAPSULA VÁRIAS INFORMAÇÕES DE UMA RESPOSTA HTTP PARA O SERVIÇO REST
+	//O PARÂMETRO {id} SERÁ JOGADO NA VARIÁVEL id, POR CONTA DISSO É NECESSÁRIO A ANOTAÇÃO @PathVariable
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = categoriaService.buscar(id);
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		//RESPOSTA OK E O OBJETO NO CORPO
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
