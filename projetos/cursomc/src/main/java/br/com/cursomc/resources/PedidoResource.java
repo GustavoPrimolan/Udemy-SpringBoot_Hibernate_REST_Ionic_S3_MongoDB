2ee754b87eacb2a90360dc6397ea872a1ca65e75
Gustavo Primolan <gustavo.primolan@hotmail.com>
2018-05-23 20:11:28 -0300

    Endpoint/ pedidos/{id} disponibilizado

---
+++ projetos/cursomc/src/main/java/br/com/cursomc/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package br.com.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cursomc.domain.Pedido;
+import br.com.cursomc.services.PedidoService;
+
+//CONTROLADOR REST E RESPONDERÁ PELO END POINT /CATEGORIAS
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService categoriaService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = categoriaService.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ projetos/cursomc/src/main/java/br/com/cursomc/services/PedidoService.java
@@ -0,0 +1,30 @@
+package br.com.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.domain.Pedido;
+import br.com.cursomc.repositories.PedidoRepository;
+import br.com.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = pedidoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public void salvarTodos(List<Pedido> listaPedidos) {
+		pedidoRepository.saveAll(listaPedidos);
+	}
+	
+}
