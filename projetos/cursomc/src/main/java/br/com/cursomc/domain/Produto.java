9cac93c4041c28da28cafb636d015dff63482639
Gustavo Primolan <gustavo.primolan@hotmail.com>
2018-05-23 19:52:56 -0300

    ItemPedido e ItemPedidoPK

--- projetos/cursomc/src/main/java/br/com/cursomc/domain/Produto.java
+++ projetos/cursomc/src/main/java/br/com/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package br.com.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -25,7 +28,6 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
-	
 	@JsonBackReference //IRÁ OMITIR A LISTA DE CATEGORIAS, POIS JÁ SERÁ BUSCADO DO OUTRO LADO
 	@ManyToMany
 	@JoinTable(name= "PRODUTO_CATEGORIA",
@@ -33,6 +35,10 @@ public class Produto implements Serializable{
 			inverseJoinColumns = @JoinColumn(name="categoria_id")) //TABELA QUE FARÁ A RELAÇÃO ENTRE AS ENTIDADES (JOIN TABLE)
 	private List<Categoria> categorias = new ArrayList<>();
 
+	//NÃO VAI TER ITEM REPETIDO NO MESMO PEDIDO
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 		
 	}
@@ -43,7 +49,15 @@ public class Produto implements Serializable{
 		this.nome = nome;
 		this.preco = preco;
 	}
-
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -75,7 +89,15 @@ public class Produto implements Serializable{
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
 
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
